cc83da1a80da9c53d9c9307bd60b520c8b8065e3
Arda Özkut <86841405+ardaozkut@users.noreply.github.com>
2023-01-11 11:07:06 +0300

    Add files via upload

---
+++ Project/basys3_ov7670_v1/basys3_ov7670_v1.cache/ip/2022.1/9/4/9453b10de68d1710/clk_wiz_0_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+// Date        : Wed Nov 30 02:33:16 2022
+// Host        : LAPTOP-CCIUAPNO running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-3
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(CLK_50, CLK_25, reset, locked, CLK_100)
+/* synthesis syn_black_box black_box_pad_pin="CLK_50,CLK_25,reset,locked,CLK_100" */;
+  output CLK_50;
+  output CLK_25;
+  input reset;
+  output locked;
+  input CLK_100;
+endmodule
